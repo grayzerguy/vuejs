eb9f8b4c41b8eaf7f45dc983dac42e7ba1bbd649
grayzerguy <grayzerg@gamil.com>
2025-07-17 17:08:55 +0300

    Add export functionality for orders to CSV and update dependencies

--- node-admin/src/controller/order.controller.ts
+++ node-admin/src/controller/order.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { getManager } from "typeorm";
 import { Order } from "../entity/order.entity";
+import { Parser } from "json2csv";
+import { OrderItem } from "../entity/order-item.entity";
 
 
 export const Orders = async (req: Request, res: Response) => {
@@ -31,3 +33,48 @@ export const Orders = async (req: Request, res: Response) => {
         }
     });
 }
+
+export const Export = async (req: Request, res: Response) => {
+    //export to csv 
+    const parser = new Parser({
+        fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']
+    });
+
+    const repository = getManager().getRepository(Order);
+    const orders = await repository.find({ relations: ['order_items'] });
+
+    const json = [];
+    orders.forEach((order: Order) => {
+        json.push({
+            ID: order.id,
+            Name: order.name,
+            Email: order.email,
+            'Product Title': '',
+            Price: '',
+            Quantity: ''
+
+        })
+        order.order_items.forEach((item: OrderItem) => {
+            json.push({
+                ID: '',
+                Name: '',
+                Email: '',
+                'Product Title': item.product_title,
+                Price: item.price,
+                Quantity: item.quantity
+            });
+        });
+    });
+    const csv = parser.parse(json)
+    res.header('Content-Type', 'text/csv');
+    res.attachment('orders.csv');
+    res.send(csv);
+}
+
+
+
+
+
+
+
+
--- node-admin/src/routes.ts
+++ node-admin/src/routes.ts
@@ -7,7 +7,7 @@ import { Permissions } from "./controller/permission.controller";
 import { CreateRole, DeleteRole, GetRole, Roles, UpdateRole } from "./controller/role.controller";
 import { CreateProduct, DeleteProduct, GetProduct, Products, UpdateProduct } from "./controller/product.controller";
 import { Upload } from "./controller/image.controller";
-import { Orders } from "./controller/order.controller";
+import { Export, Orders } from "./controller/order.controller";
 
 
 
@@ -48,6 +48,6 @@ router.post('/upload', AuthMiddleware, Upload);
 router.use('/uploads', express.static('./uploads'));
 
 router.get('/orders', AuthMiddleware, Orders);
-///klhklhklhkljh
+router.post('/export', AuthMiddleware, Export);
 
 export default router;
